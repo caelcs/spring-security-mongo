986e1d644b5592781a407f22522b9fa2d4d408e2
caelwinner <adolfoecs@gmail.com>
2014-12-28 14:15:25 -0300

    #23038476960224: Default Spring Security Mongo Configuration

---
+++ src/main/java/uk/co/caeldev/springsecuritymongo/conf/SpringSecurityMongoConfiguration.java
@@ -0,0 +1,36 @@
+package uk.co.caeldev.springsecuritymongo.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.token.ClientKeyGenerator;
+import org.springframework.security.oauth2.client.token.DefaultClientKeyGenerator;
+import org.springframework.security.oauth2.provider.token.AuthenticationKeyGenerator;
+import org.springframework.security.oauth2.provider.token.DefaultAuthenticationKeyGenerator;
+import uk.co.caeldev.springsecuritymongo.services.SecurityContextService;
+
+@Configuration
+public class SpringSecurityMongoConfiguration {
+
+    @Bean
+    public SecurityContextService securityContextService() {
+        return new SecurityContextService();
+    }
+
+    @Bean
+    public AuthenticationKeyGenerator authenticationKeyGenerator() {
+        return new DefaultAuthenticationKeyGenerator();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ClientKeyGenerator clientKeyGenerator(){
+        return new DefaultClientKeyGenerator();
+    }
+    
+}
